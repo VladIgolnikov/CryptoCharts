591a0203c29af9fc173bd1cbece83d05b59a4a03
VladIgolnikov <vladigolnikov@gmail.com>
2019-05-10 09:32:51 -0400

    merge

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const Routes = require('./routes.js');
 
 let app = express();
 let port = 3000;
@@ -10,12 +11,9 @@ let port = 3000;
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-
 app.use('/', express.static(path.join(__dirname, '/../public')));
 
-app.get('/prices', (req, res)=>{
-})
-
+app.use('/', Routes);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
